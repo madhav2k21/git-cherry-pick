7123015a37209d6087f47a492f6333208e37ddc9
madhav2k21 <madhav.anupoju.2k21@gmail.com>
2021-08-17 12:50:23 +0530

    adds cherry pick

--- greetings.sh
+++ greetings.sh
@@ -1,9 +1,5 @@
 echo "Welcome.."
-<<<<<<< HEAD
-
-||||||| parent of 8d0a381 (Reads users first name)
-=======
 echo "Enter your first name : " 
 read a
 echo "Welcome $a"
->>>>>>> 8d0a381 (Reads users first name)
+
